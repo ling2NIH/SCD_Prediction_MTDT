f6c4646867890103be0dfddc6d4400fc3e8cccf5
ling2NIH <ling2@nih.gov>
2025-05-24 18:51:53 -0400

    Delete PaddleFSL directory

--- PaddleFSL/env.sh
+++
@@ -1,3 +0,0 @@
-# Edit the following path to the correct path of PaddleFSL, for example '~/PaddleFSL'
-export PYTHONPATH=$PYTHONPATH:/DATA/renxin/FSL_all/FSL-Mate/PaddleFSL
-export SKLEARN_ALLOW_DEPRECATED_SKLEARN_PACKAGE_INSTALL=True
--- PaddleFSL/test/utils/log_test.py
+++
@@ -1,7 +0,0 @@
-from paddlefsl.utils import log
-
-if __name__ == '__main__':
-    logger = log.get_logger('root')
-    logger.info("test log")
-
-
